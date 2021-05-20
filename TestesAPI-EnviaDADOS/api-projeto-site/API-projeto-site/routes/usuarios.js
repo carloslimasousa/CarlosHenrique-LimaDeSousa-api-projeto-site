3ba1a6d0e165bbb18fee0a7c03af352ccd3e3828
carloslimasousa <carlos.lsousa3@outlook.com>
2021-05-20 13:58:06 -0300

    Validação de login

--- TestesAPI-EnviaDADOS/api-projeto-site/API-projeto-site/routes/usuarios.js
+++ TestesAPI-EnviaDADOS/api-projeto-site/API-projeto-site/routes/usuarios.js
@@ -9,10 +9,10 @@ let sessoes = [];
 router.post('/autenticar', function(req, res, next) {
 	console.log('Recuperando usuário por login e senha');
 
-	var login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+	var email = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login	
 	
-	let instrucaoSql = `select * from usuario where login='${login}' and senha='${senha}'`;
+	let instrucaoSql = `select * from cadastro where email='${email}' and senha='${senha}'`;
 	console.log(instrucaoSql);
 
 	sequelize.query(instrucaoSql, {
@@ -69,19 +69,19 @@ router.post('/cadastrar', function(req, res, next) {
 
 /* Verificação de usuário */
 router.get('/sessao/:login', function(req, res, next) {
-	let login = req.params.login;
-	console.log(`Verificando se o usuário ${login} tem sessão`);
+	let email = req.params.email;
+	console.log(`Verificando se o usuário ${email} tem sessão`);
 	
 	let tem_sessao = false;
 	for (let u=0; u<sessoes.length; u++) {
-		if (sessoes[u] == login) {
+		if (sessoes[u] == email) {
 			tem_sessao = true;
 			break;
 		}
 	}
 
 	if (tem_sessao) {
-		let mensagem = `Usuário ${login} possui sessão ativa!`;
+		let mensagem = `Usuário ${email} possui sessão ativa!`;
 		console.log(mensagem);
 		res.send(mensagem);
 	} else {
